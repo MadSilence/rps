de8161165b3d0b9ed46e9ff13783185cb2696dab
Peter <peter.kostoglod@gmail.com>
2021-10-14 13:21:53 +0300

    Some basic functions

--- game/app.js
+++ game/app.js
@@ -4,6 +4,9 @@ const ROCK = 'ROCK';
 const PAPER = 'PAPER';
 const SCISSORS = 'SCISSORS';
 const DEFAULT_USER_CHOICE = ROCK;
+const RESULT_DRAW = 'DRAW';
+const RESULT_PLAYER_WINS = 'PLAYER_WINS';
+const RESULT_COMPUTER_WINS = 'COMPUTER_WINS';
 
 
 let gameIsRunning = false;
@@ -17,6 +20,32 @@ const getPlayerChoice = function () {
     return selection;
 }
 
+const getComputerChoice = function () {
+    const randomValue = Math.random();
+    if (randomValue < 0.34) {
+        return ROCK;
+    } else if (randomValue < 0.67) {
+        return PAPER;
+    } else {
+        return SCISSORS;
+    }
+}
+
+const getWinner = function (cChoice, pChoice) {
+    if (cChoice === pChoice) {
+        return RESULT_DRAW;
+    } else if (
+        cChoice === ROCK && pChoice === PAPER ||
+        cChoice === PAPER && pChoice === SCISSORS ||
+        cChoice === SCISSORS && pChoice === ROCK
+    ) {
+        return RESULT_PLAYER_WINS;
+    } else {
+        return RESULT_COMPUTER_WINS;
+    }
+}
+
+
 startGameBtn.addEventListener('click', function () {
     if (gameIsRunning) {
         return;
@@ -24,5 +53,9 @@ startGameBtn.addEventListener('click', function () {
     gameIsRunning = true;
     console.log("Start game");
     const playerSelection = getPlayerChoice();
+    const computerChoice = getComputerChoice()
     console.log(playerSelection);
+    console.log(computerChoice);
+    const winner = getWinner(computerChoice, playerSelection);
+    console.log(winner);
 });
