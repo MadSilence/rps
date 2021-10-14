18ffb239f9e5928372b7c82393f194eeb0a71ecd
Peter <peter.kostoglod@gmail.com>
2021-10-14 12:56:30 +0300

    First basic commit

--- game/app.js
+++ game/app.js
@@ -1,1 +1,28 @@
 const startGameBtn = document.getElementById('start-game-btn');
+
+const ROCK = 'ROCK';
+const PAPER = 'PAPER';
+const SCISSORS = 'SCISSORS';
+const DEFAULT_USER_CHOICE = ROCK;
+
+
+let gameIsRunning = false;
+
+const getPlayerChoice = function () {
+    const selection = prompt(`${ROCK}, ${PAPER} or ${SCISSORS}`, '').toUpperCase();
+    if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
+        alert(`Invalid choice! We chose ${ROCK} for u`);
+        return DEFAULT_USER_CHOICE;
+    }
+    return selection;
+}
+
+startGameBtn.addEventListener('click', function () {
+    if (gameIsRunning) {
+        return;
+    }
+    gameIsRunning = true;
+    console.log("Start game");
+    const playerSelection = getPlayerChoice();
+    console.log(playerSelection);
+});
